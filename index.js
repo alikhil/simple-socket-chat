734a99c391b331ec6e6abbd2c1c97e6f98438d5e
alikhil <alikhil@mail.ru>
2015-07-03 00:14:18 +0500

    Room-протокол реализован.

--- index.js
+++ index.js
@@ -4,48 +4,76 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = 3000;
 
-var userIdNames = { };
- 
+var userIdNames = {};
+var userIdRooms = {};
+var maxUsetCount = 5;
+var roomList = { 1 : { id : 1, connected : 0, total : maxUserCount } };
+
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
+function getUserName(sock){
+    return sock.id.toString().substr(0, 5);
+}
  app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket){
 
-	var uId = socket.id.toString().substr(0,5);
-	
-	socket.on('user.register', function(user){
-		userIdNames[uId] = user.user;
-		io.emit('user.connect', { user : user.user });
+    var uId = getUserName(socket);
+    socket.emit('room.list', { rooms : Object.values(roomList) });
+
+	socket.on('room.join', function(user){
+        userIdNames[uId] = user.user;
+        if (roomList[user.roomId].connected == total) {
+            socket.emit('room.join.fail', 'room_overload');
+            socket.emit('room.list', { rooms : Object.values(roomList) });
+            return false;
+        }
+        socket.room = user.roomId;
+        userIdRooms[uId] = user.roomId;
+        this.join(user.roomId);
+		io.sockets.in(user.roomId).emit('romm.connect', { user : user.user });
 	});
 	
-	socket.on('chat.msg', function(msg){
-		userIdNames[uId] = msg.user;
-		socket.broadcast.emit('chat.msg', { user : msg.user, message : msg.message });
+	socket.on('room.chat.msg', function(msg){
+        userIdNames[uId] = msg.user;
+		socket.broadcast.to(this.room).emit('chat.msg', { user : msg.user, message : msg.message });
     });
 	
 	socket.on('disconnect', function(){
-		io.emit('user.disconnect', { user : userIdNames[uId] });
-		if(userIdNames.hasOwnProperty(uId))
-			delete(userIdNames[uId]);
+		io.sockets.in(sock.room).emit('room.leave', { user : userIdNames[uId] });
+        if (userIdNames.hasOwnProperty(uId)) {
+            delete (userIdNames[uId]);
+            delete (userIdRooms[uId]);
+        }
 	});
 	
-	socket.on('user.types', function(user) {
+	socket.on('room.user.types', function(user) {
 		userIdNames[uId] = user.user;
-		socket.broadcast.emit('user.types', { user : user.user } );
+		socket.broadcast.to(this.room).emit('room.user.types', { user : user.user } );
 	});
 	
-	socket.on('users.getOnline', function() {
+	socket.on('room.users.getOnline', function() {
 		var onlineList = [];
-		for(var user in userIdNames){
-			onlineList.push({ user : userIdNames[user]});
+        for (var user in userIdNames) {
+            if(userIdRooms[user] == this.room)
+			    onlineList.push({ user : userIdNames[user]});
 		}
-		socket.emit('users.online', { users : onlineList });
+		socket.emit('room.users.online', { users : onlineList });
 	});
 });
 
 http.listen(port, function(){
 	console.log('listening on *:' + port);
 });
+
+Object.values = function (obj) {
+    var vals = [];
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            vals.push(obj[key]);
+        }
+    }
+    return vals;
+}
